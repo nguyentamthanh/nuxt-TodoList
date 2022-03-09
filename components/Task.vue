98e9b80b362d684583e208dc63a164ebc47ddc80
nguyentamthanh <thanhh16012k@gmail.com>
2022-03-09 10:01:17 +0700

    10h

---
+++ components/Task.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="`task ${task.done ? 'is-complete' : ''}`">
+    <div class="content">{{ task.content }}</div>
+    <div class="buttons">
+      <button @click="toggleDone">{{ task.done ? 'Undo' : 'Done' }}</button>
+      <button class="delete" @click="removeTask">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexTask',
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['task'],
+  methods: {
+    toggleDone() {
+      this.$store.commit('TOGGLE_TASK', this.task);
+    },
+    removeTask() {
+      this.$store.commit('REMOVE_TASK', this.task);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
